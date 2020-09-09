3321ba70f98fd7383b0cb4fa4fe4524f44142721
Simon <german.marin@mail.escuelaing.edu.co>
2020-09-09 12:25:43 -0500

    Paralelizando y añadiendo pausa

--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinder.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimeFinder.java
@@ -4,32 +4,64 @@ import edu.eci.arsw.math.MathUtilities;
 import java.math.BigInteger;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Logger;
 
-public class PrimeFinder{
+public class PrimeFinder extends Thread{
         
 	
 	
-        
-	public static void findPrimes(BigInteger _a, BigInteger _b, PrimesResultSet prs){
-            
-                BigInteger a=_a;
-                BigInteger b=_b;
+    private boolean suspender;
+    BigInteger a,b;
+    PrimesResultSet prs;
+    public PrimeFinder(BigInteger _a, BigInteger _b, PrimesResultSet prs){
+        this.a=_a;
+        this.b=_b;
+        this.prs = prs;
+        this.suspender = true;
 
-                MathUtilities mt=new MathUtilities();
-                
-                int itCount=0;
-            
-                BigInteger i=a;
-                while (i.compareTo(b)<=0){
-                    itCount++;
-                    if (mt.isPrime(i)){
-                        prs.addPrime(i);
+
+    }
+
+    public void run(){
+        boolean flag =false;
+        int itCount=0;
+        BigInteger i=this.a;
+        MathUtilities mt=new MathUtilities();
+        while (i.compareTo(this.b)<=0){
+            System.out.println("xd");
+            synchronized(this){
+                while (isSuspender()){
+                    try {
+                        wait();
+                    } catch (InterruptedException ignored) {
+
                     }
-
-                    i=i.add(BigInteger.ONE);
                 }
-                
-	}
+
+            }
+            itCount++;
+            if (mt.isPrime(i)){
+                prs.addPrime(i);
+            }
+
+            i=i.add(BigInteger.ONE);
+        }
+    }
+    public boolean isSuspender() {
+        return suspender;
+    }
+
+    public void setSuspender(boolean suspender) {
+        this.suspender = suspender;
+    }
+    public synchronized void despertar() {
+        this.suspender=false;
+        notify();
+    }
+    public synchronized void dormir(){
+        this.suspender=true;
+    }
+
 	
 	
 	
--- PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
+++ PrimesFinderDesktopTool/src/main/java/edu/eci/arsw/primefinder/PrimesFinderTool.java
@@ -1,10 +1,13 @@
 package edu.eci.arsw.primefinder;
 
 import edu.eci.arsw.mouseutils.MouseMovementMonitor;
+
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -14,41 +17,68 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 public class PrimesFinderTool {
 
-	public static void main(String[] args) {
-		            
-            int maxPrim=1000;
-            
-            PrimesResultSet prs=new PrimesResultSet("john");
-            
-            PrimeFinder.findPrimes(new BigInteger("1"), new BigInteger("10000"), prs);
-            
-            System.out.println("Prime numbers found:");
-            
-            System.out.println(prs.getPrimes());
-            
-            
-            /*while(task_not_finished){
-                try {
-                    //check every 10ms if the idle status (10 seconds without mouse
-                    //activity) was reached. 
-                    Thread.sleep(10);
-                    if (MouseMovementMonitor.getInstance().getTimeSinceLastMouseMovement()>10000){
-                        System.out.println("Idle CPU ");
-                    }
-                    else{
-                        System.out.println("User working again!");
-                    }
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(PrimesFinderTool.class.getName()).log(Level.SEVERE, null, ex);
+    public static void main(String[] args) throws InterruptedException {
+
+        int maxPrim = 1000;
+        final int numeroHilos = 4;
+        final int rangoMaximo=10000;
+
+        PrimesResultSet prs = new PrimesResultSet("simon");
+
+        ArrayList<PrimeFinder> hilos = new ArrayList<PrimeFinder>();
+        int rango = rangoMaximo/numeroHilos;
+        int rangoInicial;
+        int rangoFinal;
+
+        for (int i = 0; i < numeroHilos; i++) {
+            if (i != numeroHilos - 1) {
+                rangoInicial = rango * i;
+                rangoFinal = rango + rangoInicial-1;
+                hilos.add(new PrimeFinder(new BigInteger(Integer.toString(rangoInicial)), new BigInteger(Integer.toString(rangoFinal)), prs));
+
+            }
+            else{
+                rangoInicial = rango * i;
+                rangoFinal = rangoMaximo;
+                hilos.add(new PrimeFinder(new BigInteger(Integer.toString(rangoInicial)), new BigInteger(Integer.toString(rangoFinal)), prs));
+            }
+//           
+        }
+//        PrimeFinder.findPrimes(new BigInteger("1"), new BigInteger("10000"), prs);
+
+        for (PrimeFinder p:hilos) {
+            p.start();
+        }
+        for (PrimeFinder p:hilos) {
+            p.despertar();
+        }
+
+
+        System.out.println("Prime numbers found:");
+
+        System.out.println(prs.getPrimes());
+
+        boolean task_not_finished = true;
+        while (task_not_finished) {
+            try {
+                //check every 10ms if the idle status (10 seconds without mouse
+                //activity) was reached.
+                Thread.sleep(10);
+                
+                if (MouseMovementMonitor.getInstance().getTimeSinceLastMouseMovement() > 10000) {
+                    System.out.println("Idle CPU ");
+                } else {
+                    System.out.println("User working again!");
                 }
-            }*/
-                        
-            
-            
-            
-            
-	}
-	
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PrimesFinderTool.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+
+    }
+
 }
 
 
